5ba5e2ab41668b9a087863ddf076583ca30890a0
Dan Nicholls <dan.nicholls@trurecognition.io>
2024-09-23 20:43:35 +1000

    feat: add Kafka producer and input data example
    - Added Kafka producer functionality in `main.go`
    - Introduced `inputgenerator` package with example JSON data
    - Updated `go.mod` and `go.sum` with new dependencies

---
+++ internal/inputgenerator/inputgenerator.go
@@ -0,0 +1,33 @@
+package inputgenerator
+
+import (
+	"embed"
+	"io"
+	"os"
+)
+
+//go:embed example.json
+var exampleJson embed.FS
+
+func readFileAsBytes(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return fileBytes, nil
+}
+
+func GetInputDataExample() ([]byte, error) {
+	content, err := exampleJson.ReadFile("example.json")
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/dannicholls/joborchestrator/internal/inputgenerator"
+	"github.com/segmentio/kafka-go"
+	"log"
+  "time"
+  "os"
+  "os/signal"
+  "syscall"
+)
+
+func ProduceMessage(client *kafka.Writer, message []byte) {
+	err := client.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: message,
+		},
+	)
+
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+}
+
+func GetNewKafkaWriter(topic string) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:                   kafka.TCP("broker:9092"),
+		Topic:                  topic,
+		AllowAutoTopicCreation: true,
+	}
+}
+
+func ProduceMessages(client *kafka.Writer, message []byte, interval time.Duration) {
+  ticker := time.NewTicker(interval)
+
+  go func() {
+    for {
+      select {
+      case t := <- ticker.C:
+          fmt.Println("Generating Message at ", t)
+          ProduceMessage(client, message)
+      }
+    } 
+  }()
+}
 
 func main() {
 	fmt.Println("Hello World!")
+
+	exampleMessage, err := inputgenerator.GetInputDataExample()
+	if err != nil {
+		fmt.Println("Error getting example data bytes: ", err)
+		return
+	}
+
+  writer := GetNewKafkaWriter("testTopic")
+  defer writer.Close()
+
+  go ProduceMessages(writer, exampleMessage, 1 * time.Second)
+
+  sigChan := make(chan os.Signal, 1)
+  signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+  <-sigChan
+
+  fmt.Println("Shutting down...")
 }

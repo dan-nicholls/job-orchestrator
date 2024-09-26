8455d960bc49995155d769439dae1d043bbc7dc2
Dan Nicholls <dan.nicholls@trurecognition.io>
2024-09-26 21:34:20 +1000

    style(main): fix indentation and formatting

--- main.go
+++ main.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/dannicholls/joborchestrator/internal/kafkalib"
-  "os"
-  "os/signal"
-  "syscall"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	fmt.Println("Welcome to Job-Orchestrator")
 
-  jm := kafkalib.NewJobManager("broker", 9092)
+	jm := kafkalib.NewJobManager("broker", 9092)
 
 	exampleJob := kafkalib.Job{
 		Name: "ExampleJob1",
 		Handler: func(message kafkalib.Message, jw *kafkalib.KafkaOutput) {
 			fmt.Printf("Processing message: %s\n", message)
-      jw.ProduceMessage([]byte("testData"), "testOuputType")
+			jw.ProduceMessage([]byte("testData"), "testOuputType")
 		},
-    Input: kafkalib.KafkaInput{
-      InputTypes: []string{"testType"},
-      Topic: "inputTopic",
-    },
-    Output: kafkalib.KafkaOutput{
-      OutputTypes: []string{"testOuputType"},
-      Topic: "inputTopic",
-    },
+		Input: kafkalib.KafkaInput{
+			InputTypes: []string{"testType"},
+			Topic:      "inputTopic",
+		},
+		Output: kafkalib.KafkaOutput{
+			OutputTypes: []string{"testOuputType"},
+			Topic:       "inputTopic",
+		},
 	}
 
 	LoggingJob := kafkalib.Job{
@@ -34,23 +34,22 @@ func main() {
 		Handler: func(message kafkalib.Message, jw *kafkalib.KafkaOutput) {
 			fmt.Printf("LOGGER - Processing message: %s\n", message.Type)
 		},
-    Input: kafkalib.KafkaInput{
-      InputTypes: []string{"testType"},
-      Topic: "inputTopic",
-    },
+		Input: kafkalib.KafkaInput{
+			InputTypes: []string{"testType"},
+			Topic:      "inputTopic",
+		},
 	}
 
-  jm.AddJob(exampleJob)
-  jm.AddJob(LoggingJob)
+	jm.AddJob(exampleJob)
+	jm.AddJob(LoggingJob)
 
+	go jm.StartJob("LoggingJob")
+	go jm.StartJob("ExampleJob")
+	defer jm.StopAllJobs()
 
-  go jm.StartJob("LoggingJob")
-  go jm.StartJob("ExampleJob")
-  defer jm.StopAllJobs()
-
-  // Enter loop until interupt signal
-  sigChan := make(chan os.Signal, 1)
-  signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-  sig := <-sigChan
-  fmt.Printf("Received signal: %s. Shutting down...\n", sig)
+	// Enter loop until interupt signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	fmt.Printf("Received signal: %s. Shutting down...\n", sig)
 }
